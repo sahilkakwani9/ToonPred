927c533aff8430fa789f6c9faf24caf1158ab695
sahilkakwani9 <sahkakwani@gmail.com>
2022-03-19 21:55:30 +0530

    Rearranged layout

--- styles/game.js
+++ styles/game.js
@@ -10,37 +10,37 @@ function playSound(character){
     cAudio.play();
 }
 
-function nextPattern(){
-    user_clicked_pattern = [];
-    var randomNum = Math.floor(Math.random()*9);
-    var randomCharacterChoice = characters[randomNum];
-    game_pattern.push(randomCharacterChoice);
-    var button = $("#"+randomCharacterChoice);
-    button.fadeOut(100).fadeIn(100);
-    playSound(randomCharacterChoice);
-}
+// function nextPattern(){
+//     user_clicked_pattern = [];
+//     var randomNum = Math.floor(Math.random()*9);
+//     var randomCharacterChoice = characters[randomNum];
+//     game_pattern.push(randomCharacterChoice);
+//     var button = $("#"+randomCharacterChoice);
+//     button.fadeOut(100).fadeIn(100);
+//     playSound(randomCharacterChoice);
+// }
 
-function checkAnswer(level){
-    if(game_pattern[level] == user_clicked_pattern[level]){
-        if(game_pattern.length == user_clicked_pattern.length){
-            setTimeout(function(){
-                nextSequence();
-            },1000);
-        }
-    }
-    else{
-        // $("body").addClass("game-over");
-        // setTimeout(() => { $("body").removeClass("game-over"); }, 2);
-        gameOver();
+// function checkAnswer(level){
+//     if(game_pattern[level] == user_clicked_pattern[level]){
+//         if(game_pattern.length == user_clicked_pattern.length){
+//             setTimeout(function(){
+//                 nextSequence();
+//             },1000);
+//         }
+//     }
+//     else{
+//         // $("body").addClass("game-over");
+//         // setTimeout(() => { $("body").removeClass("game-over"); }, 2);
+//         gameOver();
 
-    }
-}
+//     }
+// }
 
-function gameOver(){
-    game_pattern = [];
-    level = 0;
-    FirstTimePressed = true;
-}
+// function gameOver(){
+//     game_pattern = [];
+//     level = 0;
+//     FirstTimePressed = true;
+// }
 
 function animatePress(character){
     $("#"+character).addClass("pressed");
@@ -50,10 +50,10 @@ function animatePress(character){
 
 $(".col-md-4").click(function(){
     var userchosenCharacter =  this.id;
-    user_clicked_pattern.push(userchosenColour);
+    // user_clicked_pattern.push(userchosenColour);
     // userClickedPattern.push(userchosenColour);
     playSound(userchosenCharacter);
-    checkAnswer(user_clicked_pattern.length-1)
+    // checkAnswer(user_clicked_pattern.length-1)
     // animatePress(userchosenCharacter);
     // print(userchosenCharacter);
     console.log(userchosenCharacter);
@@ -64,10 +64,10 @@ $(".mod").click(function(){
     animatePress(userchosenCharacter);
 })
 
-$(document).keydown(function(){
-    // $("h1").text("Level "+level);
-    if(FirstTimePressed){
-        nextPattern();
-    }
-    FirstTimePressed = false;
-})
+// $(document).keydown(function(){
+//     // $("h1").text("Level "+level);
+//     if(FirstTimePressed){
+//         nextPattern();
+//     }
+//     FirstTimePressed = false;
+// })
