7c991586fae1c85584022644f61263fd41fc0432
umangp31 <58567586+umangp31@users.noreply.github.com>
2022-03-19 17:05:59 +0530

    Game page added

---
+++ styles/game.js
@@ -0,0 +1,6 @@
+var characters = ["bunny","devil","sylvester","wile","road-runner","elmer","porky-pig","tweety","daffy"];
+var game_pattern = [];
+var user_clicked_pattern = [];
+var score = 0;
+var FirstTimePressed = true;
+
