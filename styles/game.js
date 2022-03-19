b5091dbee289764ead6731b48c97bb186ba54f27
sahilkakwani9 <sahkakwani@gmail.com>
2022-03-19 18:14:56 +0530

    Added sounds

--- styles/game.js
+++ styles/game.js
@@ -4,3 +4,13 @@ var user_clicked_pattern = [];
 var score = 0;
 var FirstTimePressed = true;
 
+function playSound(character){
+    var cAudio = new Audio("../Sounds/"+character+'.mp3');
+    cAudio.play();
+}
+
+$(".col-md-4").click(function(){
+    var userchosenCharacter =  this.id;
+    // userClickedPattern.push(userchosenColour);
+    playSound(userchosenCharacter);
+})
